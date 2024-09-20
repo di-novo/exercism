455cdc445004069ed208a839699bb4b8c037026e
Phil Dinovo <dinovo.phil@gmail.com>
2024-09-19 23:35:45 -0700

    Passed tests for 'tisbury-treasure-hunt'

--- python/tisbury-treasure-hunt/tuples.py
+++ python/tisbury-treasure-hunt/tuples.py
@@ -8,7 +8,7 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    pass
+    return record[1]
 
 
 def convert_coordinate(coordinate):
@@ -18,7 +18,7 @@ def convert_coordinate(coordinate):
     :return: tuple - the string coordinate split into its individual components.
     """
 
-    pass
+    return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
@@ -29,7 +29,7 @@ def compare_records(azara_record, rui_record):
     :return: bool - do the coordinates match?
     """
 
-    pass
+    return convert_coordinate(get_coordinate(azara_record)) == rui_record[1]
 
 
 def create_record(azara_record, rui_record):
@@ -40,7 +40,7 @@ def create_record(azara_record, rui_record):
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
 
-    pass
+    return azara_record + rui_record if compare_records(azara_record, rui_record) else "not a match"
 
 
 def clean_up(combined_record_group):
@@ -54,4 +54,4 @@ def clean_up(combined_record_group):
     (see HINTS.md for an example).
     """
 
-    pass
+    return "\n".join(str(record[:1] + record[2:]) for record in combined_record_group) + "\n"
