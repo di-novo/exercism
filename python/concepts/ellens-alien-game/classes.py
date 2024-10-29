9b7ee6be8b3ab5e6bc000f72a7f76e3124b16b1c
Phil Dinovo <dinovo.phil@gmail.com>
2024-10-29 10:11:54 -0700

    Passed tests for 'ellens-alien-game'

--- python/concepts/ellens-alien-game/classes.py
+++ python/concepts/ellens-alien-game/classes.py
@@ -10,7 +10,7 @@ class Alien:
     x_coordinate: int - Position on the x-axis.
     y_coordinate: int - Position on the y-axis.
     health: int - Number of health points.
-
+    
     Methods
     -------
     hit(): Decrement Alien health by one point.
@@ -19,7 +19,28 @@ class Alien:
     collision_detection(other): Implementation TBD.
     """
 
-    pass
+    total_aliens_created = 0
 
+    def __init__(self,x,y):
+        self.x_coordinate = x
+        self.y_coordinate = y
+        self.health = 3
+        Alien.total_aliens_created += 1
+
+    def hit(self):
+        self.health -= 1
+
+    def is_alive(self):
+        return self.health > 0
+    
+    def teleport(self,x,y):
+        self.x_coordinate = x
+        self.y_coordinate = y
+
+    def collision_detection(self,obj):
+        pass
 
 #TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+
+def new_aliens_collection(positions_list):
+    return [Alien(x,y) for x,y in positions_list]
